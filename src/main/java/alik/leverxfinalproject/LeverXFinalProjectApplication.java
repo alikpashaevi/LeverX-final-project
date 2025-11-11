ccfec540be9810feda6a05684df0d7de2820c9e7
alikpashaevi <alikpashaev06@gmail.com>
2025-11-11 20:57:54 +0400

    feat: files upload

---
+++ src/main/java/alik/leverxfinalproject/LeverXFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package alik.leverxfinalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeverXFinalProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeverXFinalProjectApplication.class, args);
+    }
+
+}
